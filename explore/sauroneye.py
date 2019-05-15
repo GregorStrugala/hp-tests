d721bcb5ce07f2e02d3bd7088397378088bef77a
Gregor Strugala <gregor.strugala@gmail.com>
2019-05-15 15:41:25 -0400

    Correct the comparison sign in humidity function

--- explore/sauroneye.py
+++ explore/sauroneye.py
@@ -4,8 +4,8 @@ from CoolProp.HumidAirProp import HAPropsSI as psychro
 def humidity(field, exp, error):
     nconv = exp._name_converter
     def get_SI_mag(quantity, unit):
-        raw_values = exp.raw_data[nconv.loc[quantity, 'col_names']].values
-        return exp.Q_(raw_values, nconv.loc[quantity, 'units']).to(unit).magnitude
+        raw_val = exp.raw_data[nconv.loc[quantity, 'col_names']].values
+        return exp.Q_(raw_val, nconv.loc[quantity, 'units']).to(unit).magnitude
     Ts = get_SI_mag('Ts', 'K')
     RHs = get_SI_mag('RHs', 'ratio') 
     Tr = get_SI_mag('Tr', 'K')
@@ -14,6 +14,8 @@ def humidity(field, exp, error):
     ws = psychro('W', 'P', 101325, 'T', Ts, 'RH', RHs)
     wr = psychro('W', 'P', 101325, 'T', Tr, 'RH', RHr)
 
-    if (wr > ws).any():
-        error(field, 'The absolute humidity is increasing at least once.')
+    if (wr < ws).any():
+        msg = ('The supply humidity ratio exceeds '
+               'the return humidity ratio at least once.')
+        error(field, msg)
 
