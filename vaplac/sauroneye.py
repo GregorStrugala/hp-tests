c63284af7c948046a6e017a2e95a5b6bc22ebc36
Gregor Strugala <gregor.strugala@gmail.com>
2019-06-14 18:54:55 -0400

    Raise the humidity sign error tolerance

--- vaplac/sauroneye.py
+++ vaplac/sauroneye.py
@@ -10,21 +10,21 @@ import numpy as np
 # for plotting in DataTaker.validate method.
 _checkargs = {}
 
-def humidity_check(field, exp, error):
+def humidity_check(field, dtk, error):
     """Check whether the humidity ratio is increasing."""
     _checkargs['humidity_check'] = '(wr ws)'
-    wr, ws = exp.get('wr ws')
+    wr, ws = dtk.get('wr ws')
     overhum = (wr < ws).sum() / len(wr)
-    if overhum:
+    if overhum > 0.02:
         msg = ('The supply humidity ratio exceeds '
                'the return humidity ratio {:.1%} of the time.')
         error(field, msg.format(overhum))
 
-def cycling_check(field, exp, error):
+def cycling_check(field, dtk, error):
     """Check if there is cycling."""
     _checkargs['cycling_check'] = 'f'
-    varf = np.var(exp.get('f'))
-    if varf > exp.Q_('400 Hz**2'):
+    varf = np.var(dtk.get('f'))
+    if varf > dtk.Q_('400 Hz**2'):
         error(field, 'There appears to be short cycling.')
-    elif varf > exp.Q_('100 Hz**2'):
+    elif varf > dtk.Q_('100 Hz**2'):
         error(field, 'There appears to be cycling with long steps.')
