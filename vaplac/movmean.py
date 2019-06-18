42edd2cc2cea7a5256753bf988a1ff31ef8afc4a
Gregor Strugala <gregor.strugala@gmail.com>
2019-06-18 11:01:22 -0400

    Fix some typos

--- vaplac/movmean.py
+++ vaplac/movmean.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def movmean(a, n):
     """
     Compute the moving mean of an array.
@@ -23,17 +25,18 @@ def movmean(a, n):
     >>> mean = movmean(a, 20)
 
     """
-    
+
     # Increment n if it is even
     n += 1 if n % 2 == 0 else 0
     l = (n-1) // 2  # edge length
     # Compute cumulative sum
     cumsum = np.cumsum(a, dtype=float)
     # Get a moving sum everywhere but on the edges
-    movsum = np.append(cumsum[l:], np.zeros(l)) - np.append(np.zeros(l+1), cumsum[:-l-1])
+    movsum = (np.append(cumsum[l:], np.zeros(l))
+              - np.append(np.zeros(l+1), cumsum[:-l-1]))
     # Fill the edges by mirroring them and computing their moving sum
     cumsum_refl = np.cumsum(np.pad(a, (l+1, l+1), 'reflect'))
     movsum_refl = cumsum_refl[n:] - cumsum_refl[:-n]
-    movsum[:l] = mosum_refl[:l]
-    movsum[-l:] = mosum_refl[-l-1:-1]
+    movsum[:l] = movsum_refl[:l]
+    movsum[-l:] = movsum_refl[-l-1:-1]
     return movsum / n
